33899a1891df4c0a2ab00c428d9ee19e670dbebd
Yusuff Adekunle Oyedele <yoyedele@afexnigeria.com>
2023-06-11 20:41:34 +0100

    another update

--- app.js
+++ app.js
@@ -36,11 +36,7 @@ app.use(rateLimiter({
 app.use(express.json());
 
 app.use(
-    treblle({
-      apiKey: process.env.TREBLLE_API_KEY,
-      projectId: process.env.TREBLLE_PROJECT_ID,
-      additionalFieldsToMask: ["company", "position"],
-    })
+    treblle()
 )
 
 // extra packages
